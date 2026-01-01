fb9501c9fbf4012e315d26e574cc4e7b3ebc7a60
mhagel <mark.hagelberg@gmail.com>
2025-12-31 17:34:22 -0700

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -22,16 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -136,12 +126,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlue,
@@ -162,7 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onChanged: (value) {
                   // clear previous results immediately when the query changes
-                  // and reset pagination; wait 3s of inactivity before fetching
+                  // and reset pagination; wait 600ms of inactivity before fetching
                   _debounce?.cancel();
                   setState(() {
                     _query = value;
